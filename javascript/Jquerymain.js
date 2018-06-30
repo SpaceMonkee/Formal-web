56bd888f0568ea310c318afbe44dc5e3d7d34c81
SpaceMonkee <joshthebro4@gmail.com>
2018-06-30 16:55:21 +0200

    removing save option
    removing more save image as options

--- javascript/Jquerymain.js
+++ javascript/Jquerymain.js
@@ -4,4 +4,12 @@ $(document).ready(function() {
     $('#arttile').lightGallery({
         thumbnail:true
     });
-});  
+});
+
+$('body').on('contextmenu', '.lg-image', function(e){
+    e.preventDefault();
+});
+
+$("body").on("contextmenu", "img", function(e) {
+  return false;
+});
--- javascript/main.js
+++ javascript/main.js
@@ -34,3 +34,4 @@ $('a[href*="#"]')
       }
     }
   });
+// Removing download option for modal
